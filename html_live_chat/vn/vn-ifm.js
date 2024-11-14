d13bc338dee5f11bb3f81cfae5bcd5540245858b
XM <xm@XMdeMacBook-Pro.local>
2024-11-14 19:52:47 +0800

    js

--- html_live_chat/vn/vn-ifm.js
+++ html_live_chat/vn/vn-ifm.js
@@ -40,6 +40,7 @@ $(function () {
                 $('#mplayer-header').css('display', 'none');
                 $("#colse-barrage").text('Bật rào cản');
                 $('#colse-barrage').css('background-color', '#54911d');
+                $('.wp-block-group').attr('style','');
             } else {
                 $('#barrage-txt').css('display', 'block');
                 $('#send-barrage').css('display', 'block');
